996ed8c4552d45bc4892063f1a04ba9479e56de5
yassine aitoukallal <yassine.aitoukallal@gmail.com>
2024-07-23 21:51:23 +0100

    first commit

---
+++ src/oppInheritance/Cylinder.java
@@ -0,0 +1,62 @@
+package oppInheritance;
+
+public class Cylinder extends Circle {
+
+	private double height;
+	
+	public Cylinder() {
+		
+		super();
+		this.height = 1.0;
+		
+		System.out.println("Circle() + height");
+	}
+	
+	public Cylinder(double height) {
+		super();
+		this.height = height;
+		
+	}
+	
+	public Cylinder(double height , double radius) {
+		
+		super(radius);
+		this.height = height;
+		
+		
+	}
+	
+     public Cylinder(double height , double radius, String color) {
+		
+		super(radius,color);
+		this.height = height;
+		
+		
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
+	
+	public double getVolume() {
+		
+		return getArea()*this.height;
+	}
+
+	@Override
+	public String toString() {
+		return "this is a Cylinder";
+	}
+     
+     
+	
+	 
+	
+
+
+	
+}
